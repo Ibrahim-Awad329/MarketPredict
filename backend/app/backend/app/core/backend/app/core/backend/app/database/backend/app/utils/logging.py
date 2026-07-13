ff0aeea64dfce8e1ad99134e9980e5fe5b53c2a2
Ibrahim-Awad329 <awd11846@gmail.com>
2026-07-14 00:53:04 +0300

    Add logging setup using structlog

---
+++ backend/app/backend/app/core/backend/app/core/backend/app/database/backend/app/utils/logging.py
@@ -0,0 +1,11 @@
+import structlog
+
+def setup_logging():
+    structlog.configure(
+        processors=[
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.JSONRenderer()
+        ],
+        context_class=dict,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+    )
